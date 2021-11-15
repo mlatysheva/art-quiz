d03d884cd67f17b9e181d2ee360e40f0b762df1f
Maria-Latysheva <mlatysheva@gmail.com>
2021-11-15 20:11:26 +0100

    feat: add artists screen

--- art-quiz/src/index.js
+++ art-quiz/src/index.js
@@ -2,13 +2,15 @@ console.log('Time for the art quizzzz!');
 
 const pages = document.querySelectorAll('.page');
 const pageBtns = document.querySelectorAll('.page-button');
+const footer = document.getElementById('footer');
 
 pageBtns.forEach(btn => btn.addEventListener('click', function() {
   const pageName = this.dataset.page;
   this.parentNode.classList.add('hide');
   pages.forEach(page => {
     if(page.classList.contains(pageName)) {
-      page.classList.remove('hide')
+      page.classList.remove('hide');
+      // footer.classList.add('hide');
     }
   })
 }))
@@ -31,3 +33,44 @@ function increment() {
 function decrement() {
   document.getElementById('seconds-to-answer').stepDown();
 }
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex;
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+  console.log('started');
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort( () => {
+    Math.random() - 0.5
+  });
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+}
+
+function selectAnswer() {
+
+}
+
+const questions =[
+  {
+    question: 'Who is the author of this painting?',
+    answers: [
+      { text: 'Fedotov', correct: true },
+      { text: 'Murillo', correct: false }
+    ]
+  }
+]
