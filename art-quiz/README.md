7fccbe4c661632b22cf24f74ed0ac52392ae2cd0
Maria-Latysheva <mlatysheva@gmail.com>
2021-11-22 23:02:23 +0100

    doc: add screenshot

---
+++ art-quiz/README.md
@@ -0,0 +1,8 @@
+# mlatysheva-JSFE2021Q3
+Private repository for @mlatysheva
+
+Art-Quiz: https://rolling-scopes-school.github.io/mlatysheva-JSFE2021Q3/art-quiz/
+<br>
+Self-presentation: https://youtu.be/8Ranvl9cMVA
+<br>
+Momentum: https://rolling-scopes-school.github.io/mlatysheva-JSFE2021Q3/momentum/
--- art-quiz/src/js/hideScreens.js
+++ art-quiz/src/js/hideScreens.js
@@ -1,24 +1,14 @@
 function hideScreens() {
   const pages = document.querySelectorAll('.page');
   const pageBtns = document.querySelectorAll('.page-button');
-  console.log('pageBtns are: ' + pageBtns);
+  // console.log('pageBtns are: ' + pageBtns);
 
-  // pageBtns.forEach(btn => btn.addEventListener('click', function () {
-  //   const pageName = this.dataset.page;
-  //   console.log('pageName is ' + pageName);
-  //   this.parentNode.classList.add('hide');
-  //   pages.forEach(page => {
-  //     if(page.classList.contains(pageName)) {
-  //       page.classList.remove('hide');
-  //     }
-  //   })
-  // }))
 
   pageBtns.forEach(btn => btn.addEventListener('click', screenchange));
 
   function screenchange() {
     const pageName = this.dataset.page;
-    console.log('pageName is ' + pageName);
+    // console.log('pageName is ' + pageName);
     this.parentNode.classList.add('hide');
     pages.forEach(page => {
       if(page.classList.contains(pageName)) {
